916f7d634fa9440b43fe79b76055137f296c318b
Amstalden <lucas-amstalden@hotmail.com>
2022-08-01 22:59:39 -0300

    added product endpoint to api area

---
+++ WebApiProductCRUD/WebApiProductCRUD/Areas/Api/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiProductCRUD.Models;
+using WebApiProductCRUD.Repositories;
+using WebApiProductCRUD.Utils;
+
+namespace WebApiProductCRUD.Areas.Api.Controllers
+{
+    [Area("Api")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string from)
+        {
+            var dateFrom = StringDateTime.Parse(from, DateTime.Now.AddYears(-1));
+            var list = await _repository.Get(dateFrom);
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product model)
+        {
+            var result = await _repository.Edit(model);
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            var result = await _repository.Delete(id);
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+    }
+}
