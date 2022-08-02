300a6dc2a229b1e2ecac16abd1a12288cab1d0c5
Lucas Amstalden <lucas-amstalden@hotmail.com>
2022-08-02 14:41:14 -0300

    added option to use dbContext InMemory

--- WebApiProductCRUD/WebApiProductCRUD/Data/Const.cs
+++ WebApiProductCRUD/WebApiProductCRUD/Data/Const.cs
@@ -3,6 +3,8 @@
     public static class Const
     {
         public const string IntegrationEnvName = "Development";
+        public const string UseInMemoryDatabase = "UseInMemoryDatabase";
         public const string Secret = "497c9d6baa554d81b8bbff77848336c1";
+        public const string StringTrue = "true";
     }
 }
