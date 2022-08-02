1af3f035483f7adabd1ed075523c0339a89cbc3b
Lucas Amstalden <lucas-amstalden@hotmail.com>
2022-08-02 18:15:44 -0300

    added unit test project

---
+++ WebApiProductCRUD/WebApiProductCRUD.Test/ProductTests.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using WebApiProductCRUD.Data;
+using WebApiProductCRUD.Models;
+using WebApiProductCRUD.Repositories;
+
+namespace WebApiProductCRUD.Test
+{
+    public class ProductTests
+    {
+        [Fact]
+        public async Task CreateValidProduct_CreateIsCalled_ReturnValidProduct()
+        {
+            // Arrange
+            var product = new Fixture().Create<Product>();
+            product.Id = null;
+            //var appDbContextMock = new Mock<AppDbContext>();
+
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(nameof(ProductTests));
+            var dbContext = new AppDbContext(options.Options);
+            dbContext.Database.EnsureCreated();
+
+
+            var productRepository = new ProductRepository(dbContext);
+            // Act
+            var result = await productRepository.Edit(product);
+
+            //Asert
+            Assert.Equal(product.Name, result.Model.Name);
+            Assert.True(result.Model.Stock >= 0);
+        }
+    }
+}
