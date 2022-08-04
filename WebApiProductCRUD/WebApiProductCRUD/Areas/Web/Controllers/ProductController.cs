766357404fcdc4adbb0bd6455c7b14bc1a28ef5f
Amstalden <lucas-amstalden@hotmail.com>
2022-08-03 21:59:25 -0300

    added first style

---
+++ WebApiProductCRUD/WebApiProductCRUD/Areas/Web/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApiProductCRUD.Repositories;
+
+namespace WebApiProductCRUD.Areas.Web.Controllers
+{
+    [Area("Web")]
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _repository.Get();
+            return View(products.ToArray());
+        }
+
+    }
+}
